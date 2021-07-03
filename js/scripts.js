6636c576917db972f8a755211ed6498b314129aa
Marco <marcoparra311@gmail.com>
2021-07-02 22:49:16 -0400

    Update projects, add resume download button, fix contact form

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
-/*!
-* Start Bootstrap - Freelancer v6.0.6 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
+
 (function ($) {
     "use strict"; // Start of use strict
 
